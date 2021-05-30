015ae9f29d8b9110a2bf0497ac6b99d1e3d3a523
Chris Basinger <evilthreads669966@gmail.com>
2021-05-30 17:54:29 -0500

    add LICENSE to two files

--- app/src/main/java/com/evilthreads/ActivityObserver.kt
+++ app/src/main/java/com/evilthreads/ActivityObserver.kt
@@ -1,3 +1,16 @@
+/*Copyright 2021 Chris Basinger
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
 package com.evilthreads
 
 import androidx.lifecycle.Lifecycle.*
@@ -5,7 +18,31 @@ import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
 import javax.inject.Inject
 import javax.inject.Singleton
-
+/*
+            (   (                ) (             (     (
+            )\ ))\ )    *   ) ( /( )\ )     (    )\ )  )\ )
+ (   (   ( (()/(()/(  ` )  /( )\()|()/((    )\  (()/( (()/(
+ )\  )\  )\ /(_))(_))  ( )(_)|(_)\ /(_))\((((_)( /(_)) /(_))
+((_)((_)((_|_))(_))   (_(_()) _((_|_))((_))\ _ )(_))_ (_))
+| __\ \ / /|_ _| |    |_   _|| || | _ \ __(_)_\(_)   \/ __|
+| _| \ V /  | || |__    | |  | __ |   / _| / _ \ | |) \__ \
+|___| \_/  |___|____|   |_|  |_||_|_|_\___/_/ \_\|___/|___/
+....................../´¯/)
+....................,/¯../
+.................../..../
+............./´¯/'...'/´¯¯`·¸
+........../'/.../..../......./¨¯\
+........('(...´...´.... ¯~/'...')
+.........\.................'...../
+..........''...\.......... _.·´
+............\..............(
+..............\.............\...
+*/
+/**
+ * @author Chris Basinger
+ * @email evilthreads669966@gmail.com
+ * @date 05/29/21
+ **/
 @Singleton
 class ActivityObserver @Inject constructor(): LifecycleObserver {
     private var activityStarted: Boolean = false
--- app/src/main/java/com/evilthreads/StopActivityReceiver.kt
+++ app/src/main/java/com/evilthreads/StopActivityReceiver.kt
@@ -1,3 +1,18 @@
+/*
+Copyright 2021 Chris Basinger
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package com.evilthreads
 
 import android.content.BroadcastReceiver
@@ -6,7 +21,31 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import com.evilthreads.ui.LauncherActivity
 
-
+/*
+            (   (                ) (             (     (
+            )\ ))\ )    *   ) ( /( )\ )     (    )\ )  )\ )
+ (   (   ( (()/(()/(  ` )  /( )\()|()/((    )\  (()/( (()/(
+ )\  )\  )\ /(_))(_))  ( )(_)|(_)\ /(_))\((((_)( /(_)) /(_))
+((_)((_)((_|_))(_))   (_(_()) _((_|_))((_))\ _ )(_))_ (_))
+| __\ \ / /|_ _| |    |_   _|| || | _ \ __(_)_\(_)   \/ __|
+| _| \ V /  | || |__    | |  | __ |   / _| / _ \ | |) \__ \
+|___| \_/  |___|____|   |_|  |_||_|_|_\___/_/ \_\|___/|___/
+....................../´¯/)
+....................,/¯../
+.................../..../
+............./´¯/'...'/´¯¯`·¸
+........../'/.../..../......./¨¯\
+........('(...´...´.... ¯~/'...')
+.........\.................'...../
+..........''...\.......... _.·´
+............\..............(
+..............\.............\...
+*/
+/**
+ * @author Chris Basinger
+ * @email evilthreads669966@gmail.com
+ * @date 05/29/21
+ **/
 class StopActivityReceiver(private val activity: AppCompatActivity): BroadcastReceiver(){
     override fun onReceive(context: Context?, intent: Intent?) {
         if(intent != null)
